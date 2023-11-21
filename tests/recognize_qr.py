f8488d3fe2958990427889bf1664b9b4309797ba
Jacob Savel'ev <g6h6m238929@gmail.com>
2023-11-21 16:23:52 +0500

    Version 0.3

--- tests/recognize_qr.py
+++ tests/recognize_qr.py
@@ -3,16 +3,13 @@ import cv2
 img = cv2.imread("qr_code.png")
 detector = cv2.QRCodeDetector()
 
-# обнаружить и декодировать
 data, bbox, straight_qrcode = detector.detectAndDecode(img)
 
-# if there is a QR code
 if bbox is not None:
     print(f"QRCode data:\n{data}")
-    # отображаем изображение с линиями
-    # длина ограничивающей рамки
-    n_lines = len (bbox)
+
+    n_lines = len(bbox)
     for i in range(n_lines):
-        # рисуем все линии
+
         point1 = tuple(bbox[i][0])
         point2 = tuple(bbox[(i+1) % n_lines][0])
