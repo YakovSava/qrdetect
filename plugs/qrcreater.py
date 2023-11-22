0cf21d3201796f36a362c63f0a1be47e35f3e1a9
Jacob Savel'ev <g6h6m238929@gmail.com>
2023-11-22 16:38:11 +0500

    Version 0.3.1

--- plugs/qrcreater.py
+++ plugs/qrcreater.py
@@ -1,20 +1,46 @@
+from hashlib import sha1
 from typing import Any
-from qrcode import make
+from qrcode import QRCode, constants
+from PIL import Image
 
 class QRCodeGenerator:
 
-    def __init__(self, back_img:str=None):
+    def __init__(self,
+            back_img:str=None,
+            qr_model:QRCode=None
+        ):
         if not back_img:
-            self._img = False
-        else:
-            if isinstance(back_img, str):
-                self._img = True
-                self._back_img = back_img
-            else:
-                raise
-
-    def make_qr(self, data:Any=None, filename:str=None) -> str:
-        if (data is None) or (filename is None):
             raise
         else:
-            ...
+            self._center_image = Image.open(back_img)
+        if qr_model is None:
+            self._qr_model = QRCode(
+                version=1,
+                error_correction=constants.ERROR_CORRECT_L,
+                box_size=10,
+                border=4,
+            )
+        else:
+            self._qr_model = qr_model
+
+    def _made_filename(self, data:Any) -> str:
+        return sha1(data.encode()).hexdigest()
+
+    def make_qr(self, data:Any=None) -> str:
+        data = str(data)
+        filename = self._made_filename(data)
+
+        self._qr_model.add_data(data)
+        self._qr_model.make(fit=True)
+
+        qr_img = self._qr_model.make_image(fill_color="black", back_color="white")
+        qr_img = qr_img.convert("RGBA")
+
+        qr_width, qr_height = qr_img.size
+        center_width, center_height = self._center_image.size
+        center_x = int((qr_width - center_width) / 2)
+        center_y = int((qr_height - center_height) / 2)
+        qr_img.paste(self._center_image, (center_x, center_y), self._center_image)
+
+        qr_img.save(filename+".png")
+        return filename
