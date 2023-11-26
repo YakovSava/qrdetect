900509ae7b31c4cf5b293d87814818d020cddee8
Jacob Savel'ev <g6h6m238929@gmail.com>
2023-11-26 18:36:47 +0500

    Version 0.6

--- plugs/gui.py
+++ plugs/gui.py
@@ -1,4 +1,6 @@
-from customtkinter import CTk, CTkImage, CTkFrame
+from tkinter.messagebox import askokcancel
+from customtkinter import CTk, CTkLabel, CTkEntry, CTkButton,\
+    CTkOptionMenu
 
 class App(CTk):
 
@@ -6,10 +8,26 @@ class App(CTk):
         super().__init__()
 
         self.title("CityBox: QR Generator")
-        self.geometry("700x450")
+        self.geometry("500x400")
 
         self._img_path = img_path
 
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=1)
+        self._start()
+
+    def _start(self):
+        self.idlabel = CTkLabel(self, text="Введите пожалуйста ID бригады", anchor="center")
+        self.idlabel.pack(pady=(20, 10))
+
+        self.identer = CTkEntry(self, width=200)
+        self.identer.place(relx=0.5, rely=0.5, anchor="center")
+
+        self.idok = CTkButton(self, text="ОК", command=self._switch)
+        self.idok.place(relx=0.5, rely=0.5, anchor="center", y=30)
+
+    def _switch(self):
+        pass
+
+
+if __name__ == "__main__":
+    App().mainloop()
 
