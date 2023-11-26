27cd58bd61e6c25b8a064bf0192df68c02d1dd0f
Jacob Savel'ev <g6h6m238929@gmail.com>
2023-11-26 18:37:04 +0500

    Version 0.6.1

--- plugs/gui.py
+++ plugs/gui.py
@@ -25,6 +25,30 @@ class App(CTk):
         self.idok.place(relx=0.5, rely=0.5, anchor="center", y=30)
 
     def _switch(self):
+        self.idlabel.destroy()
+        self.identer.destroy()
+        self.idok.destroy()
+
+        self.exit_label = CTkLabel(self, text="Нажимать при окончании работы!")
+        self.exit_job = CTkButton(self, text="Выход", width=120, command=self._restart)
+
+        self.color_label = CTkLabel(self, text="Выберите цвет")
+        self.color_menu = CTkOptionMenu(self, values=["Розовый", "Синий", "Красный"])
+
+        self.size_label = CTkLabel(self, text="Введите размер")
+        self.size_menu = CTkEntry(
+            self)
+        self.okbut = CTkButton(self, text="Подтвердить")
+
+        self.exit_label.pack(anchor='nw', pady=10)
+        self.exit_job.pack(anchor='nw', padx=10)
+        self.color_label.pack(pady=10)
+        self.color_menu.pack(pady=10)
+        self.size_label.pack(pady=10)
+        self.size_menu.pack(pady=10)
+        self.okbut.pack(pady=10)
+
+    def _restart(self):
         pass
 
 
