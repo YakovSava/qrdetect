f88ef6b9f07e733db4f338922b1d73f702ff5db8
Jacob Savel'ev <g6h6m238929@gmail.com>
2023-12-03 22:29:58 +0500

    Version 0.12.1

--- optimize/__init__.py
+++ optimize/__init__.py
@@ -2,14 +2,14 @@ from sys import platform
 from os import listdir
 
 if platform.startswith('linux'):
-    if len(filter(lambda x: x.endswith('.so'), listdir())) != 2:
+    if len(filter(lambda x: x.endswith('.so'), listdir('./cplugs/'))) != 2:
         LIBPATH = '/lib/python'
-    from linfile import Bind
-    from connector import Connector
+    from .cplugs.linfile import Bind
+    from .cplugs.connector import Connector
 elif platform == "win32":
-    if len(filter(lambda x: x.endswith('.so'), listdir())) != 1:
+    if len(filter(lambda x: x.endswith('.so'), listdir('./cplugs/'))) != 1:
         LIBPATH = ...
-    from winfile import Bind
+    from .cplugs.winfile import Bind
 
     from requests import get
 
