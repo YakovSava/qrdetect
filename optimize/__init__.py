0c89f8ed64b008b8ab67bbf582f4792cc049ec35
Yakov Savel'ev (Phone; Infinix) <g6h6m238929@gmail.com>
2023-12-04 13:25:40 +0500

    Version 0.17

--- optimize/__init__.py
+++ optimize/__init__.py
@@ -7,7 +7,7 @@ if platform.startswith('linux'):
     from .cplugs.linfile import Bind
     from .cplugs.connector import Connector
 elif platform == "win32":
-    if len(filter(lambda x: x.endswith('.so'), listdir('./cplugs/'))) != 1:
+    if len(filter(lambda x: x.endswith('.pyd'), listdir('./cplugs/'))) != 1:
         LIBPATH = ...
     from .cplugs.winfile import Bind
 
@@ -16,9 +16,13 @@ elif platform == "win32":
     class Connector:
 
         def __init__(self):
-            ...
+            self._reqc = 0
 
         def connect(self, url:str) -> str:
+            self._reqc += 1
             return get(url).text
+            
+        def reqcount(self) -> int:
+            return self._reqc
 else:
     raise ImportError('Platform not supported!')
