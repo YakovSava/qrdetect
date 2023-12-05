0c87d8be1f4a90398b577f61ba14c189161a0045
Yakov Savel'ev (Phone; Infinix) <g6h6m238929@gmail.com>
2023-12-05 08:57:12 +0500

    Version 0.20

--- optimize/cplugs/asm/linsys.asm
+++ optimize/cplugs/asm/linsys.asm
@@ -5,7 +5,78 @@ public Cwrite
 public exists
 
 section '.filedesc' executable
+    asmfcreate:
+    ; Input:
+    ;   rax - filename
+    ;   rbx - permissions
+    ; Output:
+    ;   rax - descriptor
+        mov rcx, rbx
+        mov rbx, rax
+        mov rax, 8 ; create file 
+        int 0x80
+        ret 
+    asmfopen:
+    ; Input:
+    ;   rax - filename
+    ;   rbx - mode
+    ;       read - 0
+    ;       write - 1
+    ;       read & write - 2
+    ; Output:
+    ;   rax - descriptor
+        mov rcx, rbx
+        mov rbx, rax
+        mov rax, 5 ; open
+        int 0x80
+        ret
+    asmfclose:
+        mov rbx, rax
+        mov rax, 6 ; close
+        int 0x80
+        ret
+    asmfdelete:
+        mov rbx, rax
+        mov rax, 10 ; unlink
+        int 0x80
+        ret
+    asmfseek:
+        mov rdx, rbx
+        mov rbx, rax
+        mov rax, 19 ; seek
+        int 0x80
+    asmfread:
+        push rbx
+        push rcx
     
+        mov rbx, 1 
+        xor rcx, rcx
+        call asmfseek
+    
+        pop rcx
+        pop rbx
+    
+        mov rdx, rcx
+        mov rcx, rbx
+        mov rbx, rax
+        mov rax, 3 ; read
+        int 0x80
+    asmfwrite:
+        push rbx
+        push rcx
+    
+        mov rbx, 1 
+        xor rcx, rcx
+        call fseek
+    
+        pop rcx
+        pop rbx
+    
+        mov rdx, rcx
+        mov rcx, rbx
+        mov rbx, rax
+        mov rax, 4 ; write
+        int 0x80
 
 section '.Cread' executable
     Cread:
