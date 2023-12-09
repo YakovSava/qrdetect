52f30a6ba460e243c24f07707f68468015fa2285
Jacob Savel'ev <g6h6m238929@gmail.com>
2023-12-09 14:08:27 +0500

    Version 0.24

--- optimize/cplugs/comp.py
+++ optimize/cplugs/comp.py
@@ -1,9 +1,31 @@
+from sys import platform
 from setuptools import Extension, setup
 
-setup(
-	ext_modules=[Extension(
-		name="winfile",
-		sources=["winfile.cpp"],
-		language='c++'
-	)]			
-)
+if platform.startswith("linux"):
+	setup(
+		ext_modules=[
+			Extension(
+				name="linfile",
+				sources=["linfile.cpp"],
+				language='c++',
+				extra_objects=["linsys.o"]
+			),
+			Extension(
+				name="connector",
+				sources=["connector.cpp"],
+				language='c++'
+			)
+		]
+	)
+elif platform == "win32":
+	setup(
+		ext_modules=[
+			Extension(
+				name="winfile",
+				sources=["winfile.cpp"],
+				language='c++'
+			)
+		]
+	)
+else:
+	raise RuntimeError('Platform is not supported!')
