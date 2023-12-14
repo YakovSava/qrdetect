138eb3bc783cff47115807945b1ca340fdf19034
Jacob Savel'ev <g6h6m238929@gmail.com>
2023-12-14 20:19:21 +0500

    Version 0.28

--- plugs/__init__.py
+++
@@ -1,8 +0,0 @@
-from .cache_collector import Cache
-from .qrdecoder import Decoder
-from .connector import Connector
-from .configgetter import Config
-from .garbagecollector import Collector
-from .gui import AppScan, AppCreate
-from .qrcreater import QRCodeGenerator
-from .bind import read, write
