e948193556b72e78822aa3bd828fc0ec4c05740a
Jacob Savel'ev <g6h6m238929@gmail.com>
2023-12-27 13:15:06 +0500

    Version 0.33.1

--- plugs/ms.py
+++ plugs/ms.py
@@ -3,6 +3,9 @@ class MoySklad:
     def __init__(self):
         pass
 
+    def _get(self):
+        pass
+
     def send(self, color:str=None, size:int=None):
         if (color is None) or (size is None):
             raise
