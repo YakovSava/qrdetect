8d48f4a41afc6dd13fc9db3dd93b56e362768938
Jacob Savel'ev <g6h6m238929@gmail.com>
2024-02-10 15:48:17 +0500

    Version 0.39

--- plugs/gui.py
+++ plugs/gui.py
@@ -2,10 +2,14 @@ from tkinter.messagebox import askokcancel, showerror
 from toml import loads
 from typing import Callable, TYPE_CHECKING
 from customtkinter import CTk, CTkLabel, CTkEntry, CTkButton,\
-    CTkOptionMenu
+    CTkOptionMenu, StringVar, NORMAL, DISABLED
 
 if TYPE_CHECKING:
     from .barcode import Barcode
+
+def _set_state_for_all_widgets(widget_list, state):
+    for widget in widget_list:
+        widget.configure(state=state)
 
 class AppConfig:
 
@@ -39,6 +43,7 @@ class AppCreate(CTk):
         self.idlabel = CTkLabel(
             self, text="Введите пожалуйста ID бригады", anchor="center")
         self.idlabel.pack(pady=(20, 10))
+        self.idlabel.bind('<Return>', self._check_brigades)
 
         self.identer = CTkEntry(self, width=200)
         self.identer.place(relx=0.5, rely=0.5, anchor="center")
@@ -100,12 +105,118 @@ class AppCreate(CTk):
         else:
             self._switch()
 
-class AppScaner(CTk):
-
-    def __init__(self, barcoder:'Barcode'):
+class UnderAppScanner(CTk):
+    def __init__(self, on_close_callback):
         super().__init__()
 
-        self.title("CityBox: QR Generator")
-        self.geometry("600x500")
+        self.on_close_callback = on_close_callback
 
-        self._bar = barcoder
+        self.barcode = None
+
+        self.title("CityBox: ожидание сканирования")
+        self.geometry("200x150")  # Размер окна
+
+        self._scan()
+
+        self.protocol("WM_DELETE_WINDOW", self._exit)
+
+    def _scan(self):
+        self.enter = CTkEntry(self, placeholder_text='Отсканируйте код')
+        self.enter.pack(pady=20, padx=20)
+        self.enter.bind('<Return>', self._write_scan)
+
+    def _write_scan(self, not_used_data):
+        self.barcode = self.enter.get()
+        self._exit()
+
+    def _exit(self):
+        if self.on_close_callback:
+            self.on_close_callback(self.barcode)  # Отправляем результат назад в AppScaner
+        self.destroy()
+
+class AppScaner(CTk):
+
+    def __init__(self, send_func:Callable):
+        super().__init__()
+
+        self._send = send_func
+        self._widgets = []
+        self._temp_barcode = None
+
+        self.title("CityBox: управление списанием")
+        self.geometry("400x300")  # Размер окна
+
+        self._start()
+
+    def _sending(self):
+        status = self.status_menu.get()
+        if status == 'Произведено':
+            stat = 1
+        elif status == 'Списано':
+            stat = -1
+        else:
+            showerror('Ошибка', 'Укажите, товар произведён или списан!')
+            return
+        if self._temp_barcode is None:
+            showerror('Ошибка', 'Отсканируйте штрих-код!')
+            return
+        self._send(stat, self._temp_barcode)
+        self._temp_barcode = None
+
+    def _init_scan_code(self):
+        _set_state_for_all_widgets(self._widgets, DISABLED)
+
+        def on_scan_close(scanned_barcode):
+            _set_state_for_all_widgets(self._widgets, NORMAL)
+            self._temp_barcode = scanned_barcode
+
+        scanner = UnderAppScanner(on_close_callback=on_scan_close)
+        scanner.grab_set()  # делаем модальным
+        scanner.mainloop()
+
+    def _start(self):
+        self.idlabel = CTkLabel(
+            self, text="Введите пожалуйста ID бригады", anchor="center")
+        self.idlabel.pack(pady=(20, 10))
+        self.idlabel.bind('<Enter>', self._check_brigades)
+
+        self.identer = CTkEntry(self, width=200)
+        self.identer.place(relx=0.5, rely=0.5, anchor="center")
+
+        self.idok = CTkButton(self, text="ОК", command=self._check_brigades)
+        self.idok.place(relx=0.5, rely=0.5, anchor="center", y=30)
+
+    def _check_brigades(self):
+        if self.identer.get() not in _config.brigades:
+            showerror("CityBox: Ошибка!", "ID бригады неверен")
+        else:
+            self._scaner()
+
+    def _scaner(self):
+        self.idlabel.destroy()
+        self.identer.destroy()
+        self.idok.destroy()
+
+        self.scan_button = CTkButton(self, text="Отсканировать код", command=self._init_scan_code)
+        self.scan_button.pack(pady=20)
+
+        self.status_var = StringVar()
+
+        self.status_menu = CTkOptionMenu(self, variable=self.status_var, values=["Произведено", "Списано"])
+        self.status_var.set("Выберите статус")
+        self.status_menu.pack(pady=20)
+
+        self.login_button = CTkButton(self, text="Отправить", command=self._sending)
+        self.login_button.pack(pady=20)
+
+        self._widgets.extend([self.scan_button, self.status_menu, self.login_button])
+
+# Запуск приложения
+if __name__ == "__main__":
+    def send(*args, **kwargs):
+        print(args, **kwargs)
+
+    app = AppScaner(send)
+    #app = UnderAppScanner()
+
+    app.mainloop()
