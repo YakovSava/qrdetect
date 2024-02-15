18f7a5fb5463f8524f361f6495f8a58bd2781cc6
Jacob Savel'ev <g6h6m238929@gmail.com>
2024-02-15 21:41:34 +0500

    Version 0.2

--- plugs/ms.py
+++ plugs/ms.py
@@ -2,15 +2,35 @@ import base64
 import requests
 
 from time import sleep
-from json import loads, dumps
+from json import loads as loads_json, dumps as dumps_json
+from toml import loads as loads_toml, dumps as dumps_toml
 from typing import Callable
-from pprint import pprint # A temporary solution
-from sys import argv # A temporary solution
-from random import randint # A temporary solution
+from pprint import pprint  # A temporary solution
+from sys import argv  # A temporary solution
+from random import randint  # A temporary solution
+
+
+class _MoySkladConfigBinder:
+
+    def __init__(self, config_filename: str = 'msconfig.toml'):
+        self._filename = config_filename
+        self.config = {}
+        self._reload()
+
+    def _reload(self):
+        with open(self._filename, 'r', encoding='utf-8') as file:
+            self.config = loads_toml(file.read())
+
+    def edit(self, new_conf: dict = {}):
+        with open(self._filename, 'w', encoding='utf-8') as file:
+            file.write(dumps_toml(new_conf))
+        self._reload()
+
 
 class MoySklad:
 
-    def __init__(self, token:str="", test:bool=False):
+    def __init__(self, token: str = "", test: bool = False, binder: _MoySkladConfigBinder = _MoySkladConfigBinder()):
+        self._binder = binder
         self._token = token
         self._headers = {
             'Authorization': f'Bearer {token}',
@@ -20,37 +40,38 @@ class MoySklad:
         self._start_url = "https://api.moysklad.ru/api/remap/1.2/"
         self._is_test = test
 
-    def _get(self, method:str='entity/assortment'):
+    def _get(self, method: str = 'entity/assortment'):
         if self._is_test:
             with open('test.json', 'r', encoding='utf-8') as file:
-                return loads(file.read())
+                return loads_json(file.read())
         else:
             resp = requests.get(
-                url=self._start_url+method,
+                url=self._start_url + method,
                 headers=self._headers
             )
             return resp.json()
 
-    def _put(self, method:str='entity/product/', uuid:str='', data:dict={}):
-        return requests.put(
-            url=self._start_url+method+uuid,
-            data=dumps(data),
-            headers=self._headers
+    def _post(self, method: str = 'entity/assortment', data: dict = {}):
+        resp = requests.post(
+            url=self._start_url + method,
+            headers=self._headers,
+            data=dumps_json(data)
         )
+        return resp.json()
 
-    def _get_all_lambda(self, data:list=[], lam:Callable=lambda x: x) -> list:
+    def _get_all_lambda(self, data: list = [], lam: Callable = lambda x: x) -> list:
         return list(map(
             lam,
             data
         ))
 
-    def _filter_data(self, data:list=[], lam:Callable=lambda x: x) -> list:
+    def _filter_data(self, data: list = [], lam: Callable = lambda x: x) -> list:
         return list(filter(
             lam,
             data
         ))
 
-    def _fmap(self, x:dict):
+    def _fmap(self, x: dict):
         try:
             return {
                 'uuid': x['id'],
@@ -71,43 +92,29 @@ class MoySklad:
     def _get_normal(self) -> list[dict]:
         return self._get_all_lambda(data=self._get()['rows'], lam=self._fmap)
 
-    def get_with_barcode(self, barcode:int=0) -> dict:
+    def _test_register_product(self):
+        # data = self._get(method='entity/enter')
+        # with open('test_reg.json', 'w', encoding='utf-8') as file:
+        #     file.write(dumps(data))
+        self._get
+
+    def _test_write_downs_product(self):
+        ...
+
+    def get_with_barcode(self, barcode: int = 0) -> dict:
         if not barcode:
             return {}
         for item in self._get_normal():
             if item['barcode'] == barcode:
                 return item
 
-    def register_product(self, uuid:str='', quantity:int=0):
-        data = self._filter_data(
-            data=self._get()['rows'],
-            lam=lambda x: x['id'] == uuid
-        )[0]
-        data['quantity'] += float(quantity)
-        data['stock'] += float(quantity)
-        return self._put(
-            uuid=uuid,
-            data=data
-        ).json()
+    def register_product(self, uuid: str = '', quantity: int = 0):
+        ...
 
-    def delete(self, id: str=None):
-        if id is None:
-            raise
-        return
+    def write_downs_product(self, uuid: str = None):
+        ...
+
 
 if __name__ == '__main__':
     ms = MoySklad(token=argv[1], test=False)
-    # filtred_list = list(filter(
-    #     lambda x: x['code'] == '32121903',
-    #     ms._get()['rows']
-    # ))
-    # pprint(filtred_list)
-    # ms.register_product(uuid='0862a568-cb55-11ee-0a80-16c200085f85')
-    data = list(filter(
-        lambda x: x['barcodes'][0]['ean13'] == '2000000008356',
-        ms._get()['rows']
-    ))[0]
-    data2 = list(filter(
-        lambda x: x['code'] == '32121903',
-        ms._get()['rows']
-    ))[0]
+    ms._test_register_product()
